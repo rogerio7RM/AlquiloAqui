4c8ed24619a9d7f927c8e59ffc92f6a4a877b380
rogerio7RM <rogerio7RM@users.noreply.github.com>
2026-04-19 18:02:41 +0200

    Configure Firebase database

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 window.ALQUILO_AQUI_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAivsM0JnPcsWyJBT4QfUXYBPBHUhEcrpk",
+  authDomain: "alquiloaqui-87887.firebaseapp.com",
+  databaseURL: "https://alquiloaqui-87887-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "alquiloaqui-87887",
+  storageBucket: "alquiloaqui-87887.firebasestorage.app",
+  messagingSenderId: "367033591345",
+  appId: "1:367033591345:web:bab007cc68172617255710",
+  measurementId: "G-96TM62XDB2"
 };
 
-window.ALQUILO_AQUI_ADMIN_EMAIL = "";
+window.ALQUILO_AQUI_ADMIN_EMAIL = "contacto@alquiloaqui.com";
